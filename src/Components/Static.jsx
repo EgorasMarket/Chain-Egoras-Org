c51ea0b19aad4a351d31fe70e07f61ff791c39ff
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2024-04-19 04:51:36 +0100

    added points

--- src/Components/Static.jsx
+++ src/Components/Static.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import NumberFormat from "react-number-format";
+// import React from "react";
+// // import NumberFormat from "react-number-format";
+// import NumericFormat from "react-number-format";
 
 export const numberWithCommas = (x) => {
-  return <NumberFormat value={x} displayType="text" thousandSeparator={true} />;
+  return "yy";
 };
--- src/assets/numberWithCommas.jsx
+++ src/assets/numberWithCommas.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 // import { NumericFormat } from "react-number-format";
-import NumericFormat from "react-number-format";
+// import NumericFormat from "react-number-format";
 
 export const numberWithCommas = (x) => {
-  return (
-    <NumericFormat value={x} displayType="text" thousandSeparator={true} />
-  );
+  return ",";
+  // <NumericFormat value={x} displayType="text" thousandSeparator={true} />
 };
 
 export const colors = {
